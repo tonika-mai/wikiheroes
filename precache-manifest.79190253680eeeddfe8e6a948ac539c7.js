dc0e6e1b8082130f38d21205619c75ef574f4253
tonika-mai <monicaparvanova@gmail.com>
2020-09-14 12:15:41 +0200

    Updates

---
+++ precache-manifest.79190253680eeeddfe8e6a948ac539c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72306b9eda71627015f409d1aacc153a",
+    "url": "/wikiheroes/index.html"
+  },
+  {
+    "revision": "566741c6f20d72fef8ad",
+    "url": "/wikiheroes/static/css/main.7563458b.chunk.css"
+  },
+  {
+    "revision": "48465469091a82ae687d",
+    "url": "/wikiheroes/static/js/2.b63da1ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wikiheroes/static/js/2.b63da1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566741c6f20d72fef8ad",
+    "url": "/wikiheroes/static/js/main.840e1b51.chunk.js"
+  },
+  {
+    "revision": "3ffce1ad1a4c16b56dcd",
+    "url": "/wikiheroes/static/js/runtime-main.fe2c84f1.js"
+  },
+  {
+    "revision": "9ed60eb9157a2f6ebdfd8e997139b823",
+    "url": "/wikiheroes/static/media/browse.9ed60eb9.jpg"
+  },
+  {
+    "revision": "8e9607a53631f9e4212b16f8de9e0658",
+    "url": "/wikiheroes/static/media/femalePortrait.8e9607a5.jpg"
+  },
+  {
+    "revision": "2cc90c25aa1d27731d40ffaec73a64aa",
+    "url": "/wikiheroes/static/media/malePortrait.2cc90c25.jpg"
+  }
+]);
